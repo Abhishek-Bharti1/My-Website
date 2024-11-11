e7c07b5407d7cd46fc526d39e6d1be8033de13be
Abhishek-Bharti1 <abhishekbharti91550@gmail.com>
2024-11-11 11:44:49 +0530

    projects added

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,7 +1,10 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
-
+import Image from "next/image";
+import CheckIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import GreenImage from "@/assets/images/grain.jpg";
 const portfolioProjects = [
   {
     company: "Acme Corp",
@@ -12,7 +15,7 @@ const portfolioProjects = [
       { title: "Improved site speed by 50%" },
       { title: "Increased mobile traffic by 35%" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
+    link: "",
     image: darkSaasLandingPage,
   },
   {
@@ -24,7 +27,7 @@ const portfolioProjects = [
       { title: "Expanded customer reach by 35%" },
       { title: "Increased brand awareness by 15%" },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
+    link: "",
     image: lightSaasLandingPage,
   },
   {
@@ -36,11 +39,68 @@ const portfolioProjects = [
       { title: "Improved site speed by 50%" },
       { title: "Increased mobile traffic by 35%" },
     ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
+    link: "",
     image: aiStartupLandingPage,
   },
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className="pb-16">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">
+            Real-world Results
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 mt-4">
+          See how I transformed concepts into engaging digital experiences.
+        </p>
+        <div className="flex flex-col mt-10 gap-20">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative z-0 
+              overflow-hidden after:z-10 after:content-[''] after:absolute 
+              after:inset-0 after:outline-2 after:outline 
+              after:-outline-offset-2 after:rounded-3xl
+               after:outline-white/20 px-8 pt-8 after:pointer-events-none"
+            >
+              <div className="absolute inset-0 -z-10 opacity-5" style={{
+                backgroundImage: `url(${GreenImage.src})`
+              }}></div>
+               <div className="bg-gradient-to-r from-emerald-300 to-sky-400 
+               inline-flex font-bold uppercase 
+               tracking-widest text-sm 
+               text-transparent bg-clip-text">
+               <span>{project.company}</span>
+               <span>&bull;</span>
+               <span>{project.year}</span>
+               </div>
+              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4"/>
+              <ul className="flex flex-col gap-4 mt-4">
+                {project.results.map((result) => (
+                  <li key={result.title} className="flex items-center gap-2 text-sm text-white/50">
+                    <CheckIcon className="size-5"/>
+                    <span>{result.title}</span>
+                    </li>
+                ))}
+              </ul>
+              <a href={project.link}>
+                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                  <span>Visit Live Site</span>
+                  <ArrowUpRightIcon className="size-4"/>
+                  </button>
+              </a>
+              <Image src={project.image} alt={project.title} className="mt-8 -mb-4"/>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
